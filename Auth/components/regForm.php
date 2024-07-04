437f9af6b9dbd2b5503e19ba998d85ac9feae548
gabriel-mwash <gabrielmwash254@gmail.com>
2024-07-04 21:49:38 +0300

    some major changes

--- Auth/components/regForm.php
+++ Auth/components/regForm.php
@@ -1,15 +1,36 @@
-<form class="registration-Form">
-   <div class="group1"><img src="/Assets/images/GOK_logo.png" alt=""></div>
-   <label for="firstName">FirstName:
-      <input type="text" name="firstName">
-   </label>
-   <label for="LastName">LastName: 
-      <input type="text" name="lastName">
-   </label>
-   <label for="userEmail">Email
-      <input type="email" name="userEmail" placeholder="user@gmail.com">
-   </label>
-   <label for="userPassword">Password
-      <input type="password" name="userPassword">
-   </label>
+<form class="register-form">
+   <div class="group1">
+      <img src="/Assets/images/GOK_logo.png" alt="">
+      <p id="p">Sign in to your Account</p>
+   </div>
+   <ol>
+      <li>
+         <label for="first_name">First Name</label>
+         <input id="first_name" type="text" autofocus name="first_name" placeholder="first name here">
+      </li>
+      <li>
+         <label for="last_name">Last Name</label>
+         <input id="last_name" type="text" name="last_name" placeholder="last name here">
+      </li>
+      <li>
+         <label for="email">Email </label>
+         <input id="email" type="email" name="email" placeholder="user@gmail.com">
+      </li>
+      <li>
+         <label for="password">Password </label>
+         <input id="regPassword" type="password" name="password" placeholder="8-10 characters"
+            pattern="^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$"
+            title="password must be 8 or more characters with at least one number, an
+                  uppercase ltter, and one special character">
+      </li>
+      <li>
+         <label for="password_confirmation">Password Confirmation </label>
+         <input id="password_confirmation" type="password" autocomplete="off" 
+            name="password_confirmation" placeholder="type your password again"
+            pattern="^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W]).*$"
+            title="password must be 8 or more characters with at least one number, an 
+                  uppercase letter, and one special character">
+      </li>
+      <li><input id="signUp" type="submit" value="sign up"></li>
+   </ol>
 </form>
